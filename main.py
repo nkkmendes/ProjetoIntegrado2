ddd6198ede5e0f00d43f434cf423637f7c28c025
NKM <nkkmendes@gmail.com>
2026-09-03 17:17:25 -0300

    calculadora(divisao com resto e percentual)

--- main.py
+++ main.py
@@ -8,7 +8,22 @@ def dividir(a, b):
     else:
         return "Não é possível dividir por zero"
 
+def divisaoInteiraComResto(a,b):
+    if b != 0:
+        x=a//b
+        y=a%b
+        return(x,y)
+    else:
+        return "Não é possível dividir por zero"
 
+def percentual(a,b):
+    if b != 0:
+        x=a/b
+        x=x*100
+        return(x)
+    else:
+        return "Não é possível fazer este percentual"
+    
 n1 = float(input("Digite o primeiro número: "))
 operacao = input("Digite a operação (+, -, *, /): ")
 n2 = float(input("Digite o segundo número: "))
