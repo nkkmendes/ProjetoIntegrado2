4c16ed0b9456ca73412b0ae6729b73f890670bd4
Lucas Rago <lucasragodafonseca@gmail.com>
2026-09-03 14:41:59 -0300

    Multiplicacao e Divisao

--- main.py
+++ main.py
@@ -8,19 +8,3 @@ def dividir(a, b):
     else:
         return "Não é possível dividir por zero"
 
-
-n1 = float(input("Digite o primeiro número: "))
-operacao = input("Digite a operação (+, -, *, /): ")
-n2 = float(input("Digite o segundo número: "))
-
-
-elif operacao == "*":
-    resultado = multiplicar(n1, n2)
-
-elif operacao == "/":
-    resultado = dividir(n1, n2)
-
-else:
-    resultado = "Não vai funcionar "
-
-print(f"Resultado: {resultado}")
