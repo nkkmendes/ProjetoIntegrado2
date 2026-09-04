087db3adb42146cf8c5c2ef5a8f08b8fcc5cb943
danieloribeiro <daniel.orfaliais.ribeiro@googlemail.com>
2026-09-03 23:54:24 -0300

    Update main.py com o bloco principal e com a função de subtração e adição

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
+def calcula_adicao(x,y):
+    return x + y
+
+def calcula_subtracao(x,y):
+    return x - y
+    
 def calcula_radificacao(x,y):
     return x ** (1/y)
 
 def calcula_exponenciacao(x,y):
     return x **y
 
-def multiplicar(a, b):
+def calcula_multiplicacao(a, b):
     return a * b
 
-def dividir(a, b):
+def calcula_divisao(a, b):
     if b != 0:
         return a / b
     else:
         return "Não é possível dividir por zero"
 
-def divisaoInteiraComResto(a,b):
+def calcula_divisaoInteiraComResto(a,b):
     if b != 0:
         x=a//b
         y=a%b
@@ -21,10 +27,45 @@ def divisaoInteiraComResto(a,b):
     else:
         return "Não é possível dividir por zero"
 
-def percentual(a,b):
+def calcula_percentual(a,b):
     if b != 0:
         x=a/b
         x=x*100
         return(x)
     else:
         return "Não é possível fazer este percentual"
+
+a = int(input("""Escolha uma das opções abaixo:
+
+1 - Adição
+2 - Subtração
+3 - Mulitplicação
+4 - Divisão
+5 - Radificação
+6 - Exponenciação
+7 - Divisão inteira com resto
+8 - Percentual
+0 - Sair do programa
+"""))
+
+if a != 0:
+    x = int(input("Primeiro valor: "))
+    y = int(input("Segundo valor: "))
+    if a == 1:
+        r = calcula_adicao(x,y)
+    elif a == 2:
+        r = calcula_subtracao(x, y)
+    elif a == 3:
+        r = calcula_multiplicacao(x, y)
+    elif a == 4:
+        r = calcula_divisao(x, y)
+    elif a == 5:
+        r = calcula_radificacao(x, y)
+    elif a == 6:
+        r = calcula_exponenciacao(x, y)
+    elif a == 7:
+        r = calcula_divisaoInteiraComResto(x, y)
+    elif a == 8:
+        r = calcula_percentual(x, y)
+    print("Resultado:"+str(r))
+    
